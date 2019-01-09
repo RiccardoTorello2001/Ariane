357851348494b93792342e416aacbf39d276cf6c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 08:57:18 +0100

    ho effetuato un programma che permette di calcolare l'errore e lo spazio

--- Ariane/Program.cs
+++ Ariane/Program.cs
@@ -10,6 +10,19 @@ namespace Ariane
     {
         static void Main(string[] args)
         {
+            double errore = 0;
+            double tempo = 0;
+            double ore = 100;
+            double spazio = 0;
+            double velocità = 1676;
+            Console.WriteLine("inserire l'errore");
+            errore = Convert.ToDouble(Console.ReadLine());
+            tempo = errore * ore * 60 * 60 * 10;
+            Console.WriteLine("l'errore in secondi è {0}", tempo);
+            spazio = velocità * tempo;
+            Console.WriteLine("lo spazio è di {0}", spazio);
+            Console.ReadLine();
+
         }
     }
 }
