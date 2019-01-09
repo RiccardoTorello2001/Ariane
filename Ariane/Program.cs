2aa5ea8d19feadf723950988a69e999ebc7baf79
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:05:17 +0100

    ho fatto inserire la velocità in input

--- Ariane/Program.cs
+++ Ariane/Program.cs
@@ -14,11 +14,13 @@ namespace Ariane
             double tempo = 0;
             double ore = 100;
             double spazio = 0;
-            double velocità = 1676;
+            double velocità = 0;
             Console.WriteLine("inserire l'errore");
             errore = Convert.ToDouble(Console.ReadLine());
             tempo = errore * ore * 60 * 60 * 10;
             Console.WriteLine("l'errore in secondi è {0}", tempo);
+            Console.WriteLine("inserisci la velocità");
+            velocità = Convert.ToDouble(Console.ReadLine());
             spazio = velocità * tempo;
             Console.WriteLine("lo spazio è di {0}", spazio);
             Console.ReadLine();
